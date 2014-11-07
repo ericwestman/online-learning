abefedf66196cc125f8f4b7cbe19b1ef6194a3c1
Jane <jane@ubuntu.ubuntu-domain>
2014-11-07 14:52:11 -0500

    added visualize

---
+++ visualize.py
@@ -0,0 +1,74 @@
+import vtk
+from numpy import random
+
+dataPath1 = "data/oakland_part3_am_rf.node_features"
+dataPath2 = "data/oakland_part3_an_rf.node_features"
+
+class VtkPointCloud:
+
+    def __init__(self, zMin=-10.0, zMax=10.0, maxNumPoints=1e6):
+        self.maxNumPoints = maxNumPoints
+        self.vtkPolyData = vtk.vtkPolyData()
+        self.clearPoints()
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInput(self.vtkPolyData)
+        mapper.SetColorModeToDefault()
+        mapper.SetScalarRange(zMin, zMax)
+        mapper.SetScalarVisibility(1)
+        self.vtkActor = vtk.vtkActor()
+        self.vtkActor.SetMapper(mapper)
+
+    def addPoint(self, point):
+        if self.vtkPoints.GetNumberOfPoints() < self.maxNumPoints:
+            pointId = self.vtkPoints.InsertNextPoint(point[:])
+            self.vtkDepth.InsertNextValue(point[2])
+            self.vtkCells.InsertNextCell(1)
+            self.vtkCells.InsertCellPoint(pointId)
+        else:
+            r = random.randint(0, self.maxNumPoints)
+            self.vtkPoints.SetPoint(r, point[:])
+        self.vtkCells.Modified()
+        self.vtkPoints.Modified()
+        self.vtkDepth.Modified()
+
+    def clearPoints(self):
+        self.vtkPoints = vtk.vtkPoints()
+        self.vtkCells = vtk.vtkCellArray()
+        self.vtkDepth = vtk.vtkDoubleArray()
+        self.vtkDepth.SetName('DepthArray')
+        self.vtkPolyData.SetPoints(self.vtkPoints)
+        self.vtkPolyData.SetVerts(self.vtkCells)
+        self.vtkPolyData.GetPointData().SetScalars(self.vtkDepth)
+        self.vtkPolyData.GetPointData().SetActiveScalars('DepthArray')
+    
+    def readFile(self):
+        self.points = []
+        for i, line in enumerate((open(dataPath2, 'r'))):
+            item = line.rstrip() # strip off newline and any other trailing whitespace
+            if i>=4:
+                pos = [float(x) for x in item.split(" ")[0:3]]
+                # self.points.append(pos)
+                pointCloud.addPoint(pos)
+
+
+pointCloud = VtkPointCloud()
+pointCloud.readFile()
+
+
+# Renderer
+renderer = vtk.vtkRenderer()
+renderer.AddActor(pointCloud.vtkActor)
+renderer.SetBackground(.2, .3, .4)
+renderer.ResetCamera()
+
+# Render Window
+renderWindow = vtk.vtkRenderWindow()
+renderWindow.AddRenderer(renderer)
+
+# Interactor
+renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+renderWindowInteractor.SetRenderWindow(renderWindow)
+
+# Begin Interaction
+renderWindow.Render()
+renderWindowInteractor.Start()
