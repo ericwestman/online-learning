abdeeb3730a9364ee25e0866135ae6719ddfb5bc
Eric Westman <westman.a.eric@gmail.com>
2014-11-10 11:21:22 -0500

    Implemented a multi-class linear SVM. I think there may be something wrong with it because it performs very poorly...

--- binarySVM.m
+++ binarySVM.m
@@ -1,4 +1,4 @@
-% Kernelized SVM
+% binary SVM
 
 %% load the data from files
 clear all; close all; clc;
@@ -6,7 +6,7 @@ clear all; close all; clc;
 data1 = dlmread('data/oakland_part3_am_rf.node_features','',3,0);
 data2 = dlmread('data/oakland_part3_an_rf.node_features','',3,0);
 
-% Class label definitions: veg, wire, pole ground, facade
+% Class label definitions: veg, wire, pole, ground, facade
 classes = [1004, 1100, 1103, 1200, 1400];
 
 % First dataset
@@ -26,17 +26,22 @@ l2 = data2(:,5);
 f2 = data2(:,6:end);    % features
 
 
-%% Run Kernelized SVM
+%% Run binary SVM
 
 % Define our training data
 training_data = data1;
 T1 = length(training_data);
+
+% Define our test data
+test_data = data2;
+T2 = length(test_data);
 
 % Define number of features / classes
 F = 10;     % number of features
 C = 5;      % number of classes
 labels1 = zeros(T1,C);
 labels2 = zeros(T2,C);
+
 for c = 1:C
     labels1(:,c) = ((l1 == classes(c)) - 0.5)*2;
     labels2(:,c) = ((l2 == classes(c)) - 0.5)*2;
@@ -72,11 +77,6 @@ for iter = 1:1
     end
 end
 
-
-% Define our test data
-test_data = data2;
-T2 = length(test_data);
-
 % Define the correct labels for the test data
 label = labels2(:,4);
 pred_lab = zeros();
@@ -93,7 +93,6 @@ end
 % Display the error of the test data
 error = error / T2
 
-
 % Write data to a PCD file
 pcd = fopen('kernelizedSVM1.pcd','w');
 
@@ -103,19 +102,27 @@ end
 
 fprintf(pcd,strcat('# .PCD v.7 - Point Cloud Data file format\n',...
 'VERSION .7\n',...
-'FIELDS x y z\n',... % 'FIELDS x y z rgb\n',...
-'SIZE 4 4 4\n',... % 'SIZE 4 4 4 4\n',...
-'TYPE F F F\n',...
-'COUNT 1 1 1\n',...
-strcat(sprintf('WIDTH %d',length(x1)),'\n'),...
+'FIELDS x y z rgb\n',... % 'FIELDS x y z rgb\n',...
+'SIZE 4 4 4 4\n',... % 'SIZE 4 4 4 4\n',...
+'TYPE F F F F\n',...
+'COUNT 1 1 1 1\n',...
+strcat(sprintf('WIDTH %d',T2),'\n'),...
 'HEIGHT 1\n',...
-'VIEWPOINT 400 400 30 1 0 0 0\n',...
-strcat(sprintf('POINTS %d \n',length(x1)),'\n'),...
+'VIEWPOINT 0 0 0 1 0 0 0\n',...
+strcat(sprintf('POINTS %d \n',T2),'\n'),...
 'DATA ascii\n'));
 
-for i = 1:length(x1)
-    fprintf(pcd, '%f %f %f\n', x1(i), y1(i), z1(i));
-    %fprintf(pcd, '%d %d %d\n', x1(i), y1(i), z1(i), dot(w1,f1(t,:)));
+for i = 1:T2
+%     fprintf(pcd, '%f %f %f\n', x1(i), y1(i), z1(i));
+
+    % Set the color for each class
+    if(pred_lab(i) == 1)
+        color = 4.808e+06;
+    else
+        color = 4.2108e+06;
+    end
+    
+    fprintf(pcd, '%f %f %f %f \n', x2(i), y2(i), z2(i), color);
 end
 
 fclose(pcd);
